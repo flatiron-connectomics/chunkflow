b074fba8693e93fda711f59adffc0be94a64b7ab
Erik Schomburg <eschomburg@flatironinstitute.org>
2024-04-29 15:12:20 -0400

    add abc tooling to OperatorBase class

--- chunkflow/flow/base.py
+++ chunkflow/flow/base.py
@@ -1,9 +1,13 @@
-class OperatorBase(object):
+from abc import ABCMeta, abstractmethod
+
+
+class OperatorBase(metaclass=ABCMeta):
     """Real Operator should inherit from this base class."""
     def __init__(self, name: str = None):
         assert isinstance(name, str)
         self.name = name
 
-    def __call__(self):
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
         """The processing should happen in this function."""
         pass
