8f9bfed3a467c6a33907bb9c5c6492e8eab40e48
Erik Schomburg <eschomburg@flatironinstitute.org>
2024-10-20 19:16:06 -0400

    some improvements to neuroglancer command to add flexibility in visualizing probability maps

--- chunkflow/flow/neuroglancer.py
+++ chunkflow/flow/neuroglancer.py
@@ -287,8 +287,16 @@ void main() {
     def _append_probability_map_layer(self,
             viewer_state: ng.viewer_state.ViewerState,
             chunk_name: str, chunk: Chunk, color=None):
+
+        chunk = chunk.copy()
+
         if chunk.dtype == np.dtype('<f4') or chunk.dtype == np.dtype('float16'):
             chunk = chunk.astype(np.float32)
+        elif chunk.dtype == np.uint8:
+            chunk = chunk.astype(np.float32) / 255
+
+        if chunk.ndim == 3:
+            chunk.array = chunk.array[np.newaxis]
 
         voxel_size = self._get_voxel_size(chunk)
         # chunk = np.ascontiguousarray(chunk)
@@ -337,7 +345,7 @@ emitRGB(vec3(toNormalized(getDataValue(0)),
             shader=shader
         )
 
-    def __call__(self, datas: dict, selected: str=None, ignore_missing=False):
+    def __call__(self, datas: dict, selected: str = None, ignore_missing=False):
         """
         Parameters:
         chunks: multiple chunks
@@ -348,7 +356,7 @@ emitRGB(vec3(toNormalized(getDataValue(0)),
                 if not varname.endswith(']'):
                     raise ValueError(f"Unmatched bracket in variable name: '{varname}'")
                 varname, opts = varname[:-1].split('[')
-                for arg in opts.split(','):
+                for arg in opts.split(';'):
                     if '=' in arg:
                         k, v = arg.split('=')
                         kws[k] = v
@@ -362,7 +370,10 @@ emitRGB(vec3(toNormalized(getDataValue(0)),
         if selected is None:
             selected = datas.keys()
         elif isinstance(selected, str):
-            selected = selected.split(',')
+            if selected:
+                selected = selected.split(',')
+            else:
+                selected = []
 
         # ng.set_static_content_source(
         #     url='https://neuromancer-seung-import.appspot.com')
@@ -374,6 +385,7 @@ emitRGB(vec3(toNormalized(getDataValue(0)),
                 if name not in datas and ignore_missing:
                     continue
                 data = datas[name]
+                layer_type = layer_kwargs.pop('type', data.layer_type)
                 layer_args = (viewer_state, name, data)
                 # breakpoint()
                 
@@ -392,7 +404,7 @@ emitRGB(vec3(toNormalized(getDataValue(0)),
                     # points
                     self._append_point_annotation_layer(*layer_args, **layer_kwargs)
                 elif isinstance(data, Chunk):
-                    if data.layer_type is None:
+                    if layer_type is None:
                         if data.is_image:
                             self._append_image_layer(*layer_args, **layer_kwargs)
                         elif data.is_segmentation:
@@ -403,11 +415,11 @@ emitRGB(vec3(toNormalized(getDataValue(0)),
                             raise ValueError('affinity map is not working yet. To-Do.')
                         else:
                             raise ValueError('unsupported data type.')
-                    elif data.layer_type == 'segmentation':
+                    elif layer_type == 'segmentation':
                         self._append_segmentation_layer(*layer_args, **layer_kwargs)
-                    elif data.layer_type == 'probability_map':
+                    elif layer_type == 'probability_map':
                         self._append_probability_map_layer(*layer_args, **layer_kwargs)
-                    elif data.layer_type in set(['image', 'affinity_map']):
+                    elif layer_type in set(['image', 'affinity_map']):
                         self._append_image_layer(*layer_args, **layer_kwargs)
                     else:
                         breakpoint()
