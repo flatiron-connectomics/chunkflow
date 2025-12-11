1759c3bcd14f2f0a0df2daa543affe2dbbc70435
Erik Schomburg <eschomburg@flatironinstitute.org>
2025-12-11 16:49:16 -0500

    add voxel info and dtype args to load_mrc plugin

--- chunkflow/plugins/load_mrc.py
+++ chunkflow/plugins/load_mrc.py
@@ -1,27 +1,41 @@
-import numpy as np
-
 from chunkflow.lib.cartesian_coordinate import BoundingBox, Cartesian
 from chunkflow.chunk import Chunk
 
 import mrcfile
 
-def execute(bbox: BoundingBox, fname: str = None):
+def execute(bbox: BoundingBox = None, fname: str = None, dtype=None, voxel_size=None, voxel_size_factor=None):
 
     with mrcfile.mmap(fname) as mrc:
         # print(mrc.header)
         # print(f'volume shape: {mrc.data.shape}')
         # print(f'voxel size: {mrc.voxel_size}')
-        voxel_size = mrc.voxel_size
-        # voxel_size = [int(x/10.) for x in mrc.voxel_size]
-        # voxel_size = Cartesian.from_collection(voxel_size)
-        # print(f'voxel size: {voxel_size}')
-        arr = mrc.data[bbox.slices]
+        if voxel_size is None:
+            voxel_size = mrc.voxel_size
+            if voxel_size_factor is None:
+                voxel_size_factor = 10
+        if bbox is None:
+            arr = mrc.data
+            bbox = BoundingBox.from_delta((0, 0, 0), arr.shape)
+        else:
+            arr = mrc.data[bbox.slices]
 
-    # somehow, mrcfile pick it up with int8!
-    arr = arr.view(np.uint8)
-    z = int(voxel_size.z / 10)
-    y = int(voxel_size.y / 10)
-    x = int(voxel_size.x / 10)
-    voxel_size = Cartesian(z, y, x)
+    if isinstance(voxel_size, str):
+        try:
+            voxel_size = eval(voxel_size)
+            if isinstance(voxel_size, (int, float)):
+                voxel_size = (voxel_size, voxel_size, voxel_size)
+        except SyntaxError:
+            voxel_size = tuple(map(float, voxel_size.strip().split(' ')))
+            voxel_size_ints = tuple(map(int, voxel_size))
+            if voxel_size_ints == voxel_size:
+                voxel_size = voxel_size_ints
+    voxel_size = Cartesian.from_collection(voxel_size)
+    if voxel_size_factor:
+        voxel_size = voxel_size / voxel_size_factor
+    print(f'voxel size: {voxel_size}')
+
+    if dtype:
+        arr = arr.view(dtype)
+
     chunk = Chunk(arr, voxel_offset=bbox.start, voxel_size=voxel_size)
     return chunk 
